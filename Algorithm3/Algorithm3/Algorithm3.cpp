497ad5d21753d3a3f6504c7e0ee85776cdc5f9cb
whdcumt <wanghongdong0@126.com>
2016-01-26 07:58:56 +0800

    修改代码，程序可运行

--- Algorithm3/Algorithm3/Algorithm3.cpp
+++ Algorithm3/Algorithm3/Algorithm3.cpp
@@ -15,7 +15,11 @@ int *boxesForGauss(float sigma, int n);  // standard deviation, number of boxes
 //模糊算法2函数声明
 void gaussBlur_3(IplImage *scl,IplImage *tcl,int w,int h,int r);
 //均值模糊函数声明
-void boxBlur_2(IplImage *scl,IplImage *tcl,int w,int h,int r); 
+void boxBlur_3(IplImage *scl,IplImage *tcl,int w,int h,int r); 
+//水平一维模糊函数声明
+void boxBlurH_3 (IplImage *scl,IplImage *tcl,int w,int h,int r);
+//竖直一维模糊函数声明
+void boxBlurT_3(IplImage *scl,IplImage *tcl,int w,int h,int r);
 int _tmain(int argc, _TCHAR* argv[])
 {
 
@@ -44,9 +48,9 @@ int _tmain(int argc, _TCHAR* argv[])
 	IplImage* gImgBlur=cvCreateImage(cvGetSize(InputImageConvertIp),IPL_DEPTH_8U,1);  
 	IplImage* bImgBlur=cvCreateImage(cvGetSize(InputImageConvertIp),IPL_DEPTH_8U,1);  
 	DWORD start_time=GetTickCount();
-	gaussBlur_3(rImg, rImgBlur,rImgBlur->width ,rImgBlur->height,10);
-	gaussBlur_3(gImg, gImgBlur,gImgBlur->width ,gImgBlur->height,10);
-	gaussBlur_3(bImg, bImgBlur,bImgBlur->width ,bImgBlur->height,10);
+	gaussBlur_3(rImg, rImgBlur,rImgBlur->width ,rImgBlur->height,3);
+	gaussBlur_3(gImg, gImgBlur,gImgBlur->width ,gImgBlur->height,3);
+	gaussBlur_3(bImg, bImgBlur,bImgBlur->width ,bImgBlur->height,3);
 	DWORD end_time=GetTickCount();
     cout<<"The run time is:"<<(end_time-start_time)<<"ms!"<<endl;//输出运行时间
    // cvShowImage("reddeblur",rImgBlur);
@@ -89,35 +93,10 @@ double round(double r)
 void gaussBlur_3(IplImage *scl,IplImage *tcl,int w,int h,int r)
 {
     int *bxs = boxesForGauss(r, 3);
-    boxBlur_2 (scl, tcl, w, h, (bxs[0]-1)/2);
-    boxBlur_2 (tcl, scl, w, h, (bxs[1]-1)/2);
-    boxBlur_2 (scl, tcl, w, h, (bxs[2]-1)/2);
+    boxBlur_3 (scl, tcl, w, h, (bxs[0]-1)/2);
+    boxBlur_3 (tcl, scl, w, h, (bxs[1]-1)/2);
+    boxBlur_3 (scl, tcl, w, h, (bxs[2]-1)/2);
 }
-void boxBlur_2(IplImage *scl,IplImage *tcl,int w,int h,int r) 
-{
-    CvScalar s;
-	for(int i=0; i<h; i++)
-        for(int j=0; j<w; j++) 
-		{
-            int val = 0;
-			int x=0,y=0;
-			for(int iy=i-r; iy<i+r+1; iy++)
-                for(int ix=j-r; ix<j+r+1; ix++) 
-				{
-                     x = MIN(w-1, MAX(0, ix));
-                     y = MIN(h-1, MAX(0, iy));
-                    val += cvGet2D(scl,y,x).val[0];
-                }
-			s.val[0]=val/((r+r+1)*(r+r+1));
-            cvSet2D(tcl,i,j, s); 
-        }
-}
-
-
-
-
-
-
 void boxBlur_3 (IplImage *scl,IplImage *tcl,int w,int h,int r)
 {
     boxBlurH_3(scl, tcl, w, h, r);
@@ -125,29 +104,33 @@ void boxBlur_3 (IplImage *scl,IplImage *tcl,int w,int h,int r)
 }
 void boxBlurH_3 (IplImage *scl,IplImage *tcl,int w,int h,int r) 
 {
- for(int i=0; i<h; i++)
+    CvScalar s;
+	for(int i=0; i<h; i++)
         for(int j=0; j<w; j++)  
 		{
-            int  val = 0;
+            double  val = 0;
             for(int ix=j-r; ix<j+r+1; ix++) 
 			{
                  int x = MIN(w-1, MAX(0, ix));
-                 val += scl[i*w+x];
+                 val += cvGet2D(scl,i,x).val[0];
             }
-            tcl[i*w+j] = val/(r+r+1);
+			s.val[0]=val/(r+r+1);
+            cvSet2D(tcl,i,j, s);
         }
 }   
 void boxBlurT_3(IplImage *scl,IplImage *tcl,int w,int h,int r) 
 {
-  for(var i=0; i<h; i++)
-        for(var j=0; j<w; j++) 
+  CvScalar s;
+  for(int i=0; i<h; i++)
+        for(int j=0; j<w; j++) 
 		{
-            var val = 0;
-            for(var iy=i-r; iy<i+r+1; iy++) 
+            double val = 0;
+            for(int iy=i-r; iy<i+r+1; iy++) 
 			{
-                var y = Math.min(h-1, Math.max(0, iy));
-                val += scl[y*w+j];
+                int y = MIN(h-1, MAX(0, iy));
+                val += cvGet2D(scl,y,i).val[0];;
             }
-            tcl[i*w+j] = val/(r+r+1);
+			s.val[0]=val/(r+r+1);
+            cvSet2D(tcl,i,j, s);
         }
 }
